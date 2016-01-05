9d6ab7dbbb9e324e4596d5dc006646405ea776cd
LEXXiY <lexxiy@mail.ru>
2016-01-05 14:13:40 +0200

    Переработали функцию print_cart, добавили get_int(), переработали diskont()

--- lvl4/dz4.php
+++ lvl4/dz4.php
@@ -30,53 +30,67 @@ diskont = diskont' . mt_rand(0, 2) . ';
 $in_cart = parse_ini_string($ini_string, true);
 print_arr($in_cart);
 
-function print_cart($in_cart){
-	$cart = '<table border="1"><tr><td><b>Наименование</b></td><td><b>Количество (на складе)</b></td><td><b>Цена</b></td><td><b>Итого</b></td></tr>';
-	$total_goods = 0;
-	$total_cost = 0;
+function print_cart(){
+	global $in_cart;
+	$cart = '<table border="1"><tr><td><b>Наименование</b></td><td><b>Количество</b></td><td><b>Цена</b></td><td><b>Итого</b></td></tr>';
+	static $total_goods = 0;
+	static $total_cost = 0;
+	static $count = 0;
 
 	foreach($in_cart as $key => $val) {
+
+
+
 		$cart .= '<tr><td>' . $key . '</td>';
-		$cart .= '<td>' . stock($val['количество заказано'], $val['осталось на складе']) . '</td>';
-		$cart .= '<td>' . $val['цена'] . '</td>';
-		$cart .= '<td>';
 
-		if (stock($val['количество заказано'], $val['осталось на складе'])>=$val['количество заказано']) {
-			if ($key == 'игрушка детская велосипед' && stock($val['количество заказано'], $val['осталось на складе']) >= 3) {
-				$cart .= round($val['цена'] / 1.3 * $val['количество заказано']);
-			} else {
-				$cart .= round($val['цена'] / diskont($val['diskont']) * $val['количество заказано']);
-			}
+		if( stock( $val['количество заказано'], $val['осталось на складе'] ) ) {
+
+			$cart .= '<td>';
+
+			$cart .= $val['количество заказано'];
+
+			$cart .= '</td>';
+			$cart .= '<td>' . $val['цена'] . '</td>';
+			$cart .= '<td>';
+
+			$price = get_price( $val['цена'], $val['diskont'], $val['количество заказано'], $key );
+
+			$cart .= $price;
+
+			$cart .= '</td></tr>';
+
+			$count++;
+			$total_goods += $val['количество заказано'];
+			$total_cost += $price;
+
 		} else {
-			echo '-';
+			$cart .= '<td>К сожалению на складе только ' . $val['осталось на складе']. '</td><td>-</td><td>-</td></tr>';
 		}
-
-		$total_goods += $val['количество заказано'];
-		$total_cost += $val['количество заказано'] * $val['цена'];
 
 	}
 	$cart .= '</table>';
-	$cart .= '<p>ИТОГО: ' . count($in_cart) . ' наименования, ' . $total_goods . ' товаров<br/>';
-	$cart .= 'На сумму ' . $total_cost . '';
+	$cart .= '<p>ИТОГО: ' . $count . ' наименования, ' . $total_goods . ' товаров, ' . 'на сумму ' . $total_cost;
 
 	return $cart;
 }
 
-echo print_cart($in_cart);
+echo print_cart();
 
 /* Helpers */
 
-function stock($stock, $available){
+function stock($order, $available){
 
-	if($stock>$available) return 'На складе ' . $available;
+	if($order<=$available) return true;
 
-	if($stock<=$available) return $stock;
+	if($order>$available) return false;
 
 }
 
-function diskont($diskont){
+function diskont($diskont, $key = '', $available){
 
-	$diskont = preg_replace("/[^0-9]/", '', $diskont);
+	if ($key = 'игрушка детская велосипед' && $available >= 3) return 1.3;
+
+	$diskont = get_int($diskont);
 
 	if ($diskont == 0) return 1;
 
@@ -90,9 +104,20 @@ function diskont($diskont){
 	}
 }
 
+function get_price($price, $diskont=1, $available, $name){
+
+		return round($price / diskont($diskont, $name, $available) * $available, 2);
+
+}
+
 /* Library */
 function print_arr($array){
 	echo "<pre>";
 	print_r($array);
 	echo "</pre>";
+}
+
+function get_int($string){
+	$int = preg_replace("/[^0-9]/", '', $string);
+	return $int;
 }
