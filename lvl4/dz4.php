b663a89b2c70098fc85c6b96f03e246e748f049f
lexxiy <lexxiy@gmail.com>
2016-01-07 14:23:59 +0000

    Добавлен расчет выгоды от покупки акционного товара, а так же проверки на возможность участия в акциях, выведен итоговый расчет по заказу с указанием сэкономленной суммы

--- lvl4/dz4.php
+++ lvl4/dz4.php
@@ -29,23 +29,33 @@ diskont = diskont' . mt_rand(0, 2) . ';
 ';
 
 $in_cart = parse_ini_string($ini_string, true);
+var_dump($in_cart);
 
 function print_cart(){
 	global $in_cart;
 	
-	if ( array_key_exists('игрушка детская велосипед', $in_cart) && $in_cart['игрушка детская велосипед']['количество заказано'] >= 3 ) {
-		echo '<span style="color:red">АКЦИЯ!</span><br/> Поздравляем вас, купив товар "игрушка детская велосипед" количеством более 3 штук, вы получаете скидку в 30%';
-		$in_cart['игрушка детская велосипед']['diskont'] = 'diskont3';
-	} else {
-		echo 'Купив товар "игрушка детская велосипед" количеством более 3 штук вы получите скидку 30%!';
-	}
+	if ( array_key_exists('игрушка детская велосипед', $in_cart) ) {
+		
+		if( get_available($in_cart['игрушка детская велосипед']['количество заказано'], $in_cart['игрушка детская велосипед']['осталось на складе']) >= 3 ) {
+			
+			echo '<p>Поздравляем вас с участием в акции, вы купили товар "игрушка детская велосипед" в количестве более 3 штук, и получаете скидку в 30%</p>';
+			$in_cart['игрушка детская велосипед']['diskont'] = 'diskont3';
+			
+		} else {
+			echo 'Купив товар "игрушка детская велосипед" количеством более 3 штук вы получите скидку 30%!';
+		}
+		
+		$action_profit = get_profit($in_cart['игрушка детская велосипед']['количество заказано'], $in_cart['игрушка детская велосипед']['осталось на складе'], $in_cart['игрушка детская велосипед']['цена']);
+	} 
 	
 	$cart = '<table border="1"><tr>';
 	$cart .= '<td>№</td>' . '<td><b>Наименование</b></td>' . '<td><b>Цена</b></td>' . '<td><b>Скидка</b></td>';
-	$cart .= '<td><b>В заказе</b></td>' . '<td><b>На складе</b></td>' . '<td><b>Итого</b></td></tr>';
+	$cart .= '<td><b>В заказе</b></td>' . '<td><b>На складе</b></td>' . '<td><b>Итого со скидкой</b></td></tr>';
+	
 	static $total_goods = 0;
 	static $total_cost = 0;
 	static $count = 0;
+	static $total_profit = 0;
 
 	foreach($in_cart as $key => $val) {
 
@@ -53,6 +63,7 @@ function print_cart(){
 		$in_order = $val['количество заказано'];
 		$in_stock = $val['осталось на складе'];
 		$diskont = get_int($val['diskont']);
+		$show_diskont = ($diskont>0) ? $diskont.'0%' : '0%'; //красивое число скидки в таблице
 		$price = get_price( $item_price, $diskont, $in_order, $key, $in_stock );
 
 		$cart .= '<tr><td>' . ++$count . '</td>';
@@ -61,24 +72,29 @@ function print_cart(){
 
 		$cart .= '<td>' . $item_price . '</td>';
 
-		$cart .= '<td>' . $diskont . '0%</td>';
+		$cart .= '<td>' . $show_diskont . '</td>';
 
 		$cart .= '<td>' . $in_order . '</td>';
 
 		$cart .= '<td>' . $in_stock . '</td>';
 
-		$cart .= '<td>' . $price . '</td>';
+		$cart .= '<td>' . $price[0] . '</td>';
 
 		$cart .= '</tr>';
 
-		$total_goods += $in_order;
-		$total_cost += $price;
+		$total_goods += get_available($in_order, $in_stock);
+		$total_cost += $price[0];
+		$total_profit += $price[1]-$price[0];
 
 	}
 	
 	$cart .= '</table>';
-	$cart .= '<p>ИТОГО: ' . $count . ' наименования, ' . $total_goods . ' товаров, ' . 'на сумму ' . $total_cost;
-
+	
+	$cart .= $action_profit;
+	
+	$cart .= '<table style="border:1px solid gray;background:lightgray"><tr><td>ИТОГО в заказе:</td><td> - ' . $count . ' наименования,<br/>';
+	$cart .= '- ' . $total_goods . ' товаров,<br/> - ' . $total_cost . ' сумма к оплате,<br/> - ' . $total_profit . ' сэкономлено на скидках</td></tr></table>';
+	
 	return $cart;
 }
 
@@ -88,9 +104,8 @@ echo print_cart();
 
 /* Helpers */
 
-function diskont($diskont, $key = '', $goods){
 
-	if ($key = 'игрушка детская велосипед' && $goods >= 3) return 1.3;
+function diskont($diskont, $key = '', $goods){
 
 	if ($diskont == 0) return 1;
 
@@ -101,23 +116,36 @@ function diskont($diskont, $key = '', $goods){
 		case 2:
 			return 1.2;
 			break;
+		case 3:
+			return 1.3;
+			break;
 	}
 }
 
 function get_price($price, $diskont=1, $in_order, $name, $in_stock){
 	
-	$available = ($in_order<=$in_stock) ? $in_order : $in_stock;
+	$available = get_available($in_order, $in_stock);
 	
 	if ($available > 0) {
 		
 		$get_diskont = diskont($diskont, $name, $available);
 
-		return round($price / $get_diskont * $available, 2);
+		return array(round($price / $get_diskont * $available, 2), round($price * $available, 2));
 		
 	} else {
-		return 0;
+		return array('Товара нет в наличии');
 	}
 
+}
+
+function get_available($in_order, $in_stock){
+	return ($in_order<=$in_stock) ? $in_order : $in_stock;
+}
+
+function get_profit($in_order, $in_stock, $price){
+	if ($in_stock >= 3 && $in_order < 3 ) {
+		return '<p style="border:1px dotted orange;color:orange">Добавив в корзину еще ' . (3-$in_order) . ' товар "игрушка детская велосипед" вы получите скидку 30% и сэкономите ' . round (3 * $price / 1.3 - $in_order*$price) . '$.</p>';
+	}
 }
 
 /* Library */
