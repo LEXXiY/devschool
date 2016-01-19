e3e025f43634a87d4516b8175d3367bcf4c1490c
lexxiy <lexxiy@gmail.com>
2016-01-19 14:13:21 +0000

    Add prepareAd function

--- lvl6/dz6.php
+++ lvl6/dz6.php
@@ -8,13 +8,17 @@ ini_set('display_errors', 1);
 
 session_start();
 if( !empty($_POST) ) {
-    if(isset($_GET['action']) && $_GET['action']=='update'){
-        $id = (isset($_POST['id'])) ? $_POST['id'] : '';
+    
+    $id = (isset($_POST['id'])) ? $_POST['id'] : '';
+    
+    if(isset($_GET['action']) && $_GET['action']=='update' && isset($_SESSION['ads'][$id])){
         
-        $_SESSION['ads'][$id] = $_POST;
+        $_SESSION['ads'][$id] = prepareAd($_POST);
 
     } else {
-        $_SESSION['ads'][] = $_POST;
+        
+        $_SESSION['ads'][] = prepareAd($_POST);
+        
     }
     
 } elseif( isset($_GET['del']) ){
@@ -22,18 +26,11 @@ if( !empty($_POST) ) {
     unset($_SESSION['ads'][$_GET['del']]);
     
 } elseif( isset($_GET['edit']) && !isset($_GET['action']) ){
-    $id = $_GET['edit'];
     
-    $forma = ($_SESSION['ads'][$id]['forma']==1) ? 1 : 0;
-    $seller_name = ($_SESSION['ads'][$id]['seller_name']) ? $_SESSION['ads'][$id]['seller_name'] : '';
-    $email = ($_SESSION['ads'][$id]['email']) ? $_SESSION['ads'][$id]['email'] : '';
-    $newsletter = ($_SESSION['ads'][$id]['newsletter']==1) ? 1 : 0;
-    $phone = ($_SESSION['ads'][$id]['phone']) ? $_SESSION['ads'][$id]['phone'] : '';
-    $location_id = ($_SESSION['ads'][$id]['location_id']) ? $_SESSION['ads'][$id]['location_id'] : '';
-    $category_id = ($_SESSION['ads'][$id]['category_id']) ? $_SESSION['ads'][$id]['category_id'] : '';
-    $title = ($_SESSION['ads'][$id]['title']) ? $_SESSION['ads'][$id]['title'] : '';
-    $description = ($_SESSION['ads'][$id]['description']) ? $_SESSION['ads'][$id]['description'] : '';
-    $price = ($_SESSION['ads'][$id]['price']) ? $_SESSION['ads'][$id]['price'] : '';
+    $id = $_GET['edit'];
+   
+    $edit_form = prepareAd($_SESSION['ads'][$id]);
+
 }
 
 ?>
@@ -52,33 +49,33 @@ if( !empty($_POST) ) {
         <div class="form-group">
 
             <label class="col-sm-5 col-sm-offset-1 radio-inline">
-                <input type="radio" <?php if(isset($forma) && $forma===1) echo 'checked="checked"'; ?> value="1" name="forma">Частное лицо
+                <input type="radio" <?php if(isset($edit_form['forma']) && $edit_form['forma'] === 1) echo 'checked="checked"'; ?> value="1" name="forma">Частное лицо
             </label>
             <label class="col-sm-5  radio-inline">
-                <input type="radio" <?php if(isset($forma) && $forma===0) echo 'checked="checked"'; ?> value="0" name="forma">Компания
+                <input type="radio" <?php if(isset($edit_form['forma']) && $edit_form['forma'] === 0) echo 'checked="checked"'; ?> value="0" name="forma">Компания
             </label>
         </div>
         
         <div class="form-group">
             <label for="fld_seller_name" class="form-label col-sm-5"><b id="your-name">Ваше имя</b></label>
-            <input type="text" maxlength="40" class="col-sm-7" value="<?php if(isset($seller_name)) echo $seller_name; ?>" name="seller_name" id="fld_seller_name">
+            <input type="text" maxlength="40" class="col-sm-7" value="<?php if(isset($edit_form['seller_name'])) echo $edit_form['seller_name']; ?>" name="seller_name" id="fld_seller_name">
         </div>
 
         <div class="form-group">
             <label for="fld_email" class="form-label col-sm-5">Электронная почта</label>
-            <input type="text" class="col-sm-7" value="<?php if(isset($email))  echo $email; ?>" name="email" id="fld_email">
+            <input type="text" class="col-sm-7" value="<?php if(isset($edit_form['email']))  echo $edit_form['email']; ?>" name="email" id="fld_email">
         </div>
         
         <div class="form-group">
             <label class="col-sm-12" for="allow_mails">
-                <input type="checkbox" name="newsletter" value="1" <?php if(isset($newsletter) && $newsletter===1) echo 'checked="checked"'; ?> id="allow_mails">
+                <input type="checkbox" name="newsletter" value="1" <?php if(isset($edit_form['newsletter']) && $edit_form['newsletter'] === 1) echo 'checked="checked"'; ?> id="allow_mails">
                 <span>Я не хочу получать вопросы по объявлению по e-mail</span>
             </label>
         </div>
         
         <div class="form-group">
             <label for="fld_phone" class="form-label col-sm-5">Номер телефона</label>
-            <input type="text" class="col-sm-7" value="<?php if(isset($phone)) echo $phone; ?>" name="phone" id="fld_phone">
+            <input type="text" class="col-sm-7" value="<?php if(isset($edit_form['phone'])) echo $edit_form['phone']; ?>" name="phone" id="fld_phone">
         </div>
 
         <div id="f_location_id" class="form-group">
@@ -107,17 +104,17 @@ if( !empty($_POST) ) {
 
         <div id="f_title" class="form-group f_title">
             <label for="fld_title" class="form-label">Название объявления</label>
-            <input type="text" maxlength="50" class="form-control" value="<?php if(isset($title)) echo $title; ?>" name="title" id="fld_title">
+            <input type="text" maxlength="50" class="form-control" value="<?php if(isset($edit_form['title'])) echo $edit_form['title']; ?>" name="title" id="fld_title">
         </div>
         
         <div class="form-group">
             <label for="fld_description" class="form-label" id="js-description-label">Описание объявления</label>
-            <textarea maxlength="3000" name="description" id="fld_description" class="form-control"><?php if(isset($description)) echo $description; ?></textarea>
+            <textarea maxlength="3000" name="description" id="fld_description" class="form-control"><?php if(isset($edit_form['description'])) echo $edit_form['description']; ?></textarea>
         </div>
         
         <div id="price_rw" class="form-group rl">
             <label id="price_lbl" for="fld_price" class="form-label">Цена</label>
-            <input type="text" maxlength="9" class="form-input-text-short" value="<?php if(isset($price)) echo $price; ?>" name="price" id="fld_price">&nbsp;<span id="fld_price_title">руб.</span>
+            <input type="text" maxlength="9" class="form-input-text-short" value="<?php if(isset($edit_form['price'])) echo $edit_form['price']; ?>" name="price" id="fld_price">&nbsp;<span id="fld_price_title">руб.</span>
         </div>
         <input type="hidden" name="id" value="<?php if(isset($_GET['edit'])) echo $_GET['edit']?>"/>
         <div class="col-sm-12">
